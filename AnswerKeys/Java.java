f50b0fd46e522710bf677047c809f7c2cc1f6530
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2024-10-29 23:37:58 -0400

    Create Java.java

---
+++ AnswerKeys/Java.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Java {
+  public static void main(String[] args) {
+    // Create Scanner for input
+    Scanner in = new Scanner(System.in);
+
+    // Ask for favorite animal
+    System.out.print("What is your favorite animal? ");
+    String favAnimal = in.next();
+
+    // Ask for favorite number
+    System.out.print("What is your favorite number? ");
+    int favNumber = in.nextInt();
+
+    // Print "Hello, World!" message
+    System.out.println("Hello world! My favorite animal is " + animal + "! My favorite number is " + favNumber + "!");
+  }    
+}
