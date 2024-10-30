6eaa7bd91b03bfd024b8f1e2cb2babc88258f0fa
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2024-10-29 23:27:10 -0400

    Update and rename Python to Python.py

---
+++ AnswerKeys/Python.py
@@ -0,0 +1,8 @@
+# Choose favorite number
+favNumber = 30
+
+# Ask for favorite college
+favCollege = input("What is your favorite college or university? ")
+
+# Print message adding context to these variables
+print("My favorite college is", favCollege, "and my favorite number is," favoriteNumber, ".")
