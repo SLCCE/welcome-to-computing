8e34e3c8c898394f619c44374a2d6e9da11fddb3
Jonathan Rogers <39242954+jonathan-rogers-dev@users.noreply.github.com>
2024-11-09 14:39:58 -0500

    Update Python.py

--- StarterCode/Python.py
+++ StarterCode/Python.py
@@ -1,4 +1,8 @@
-# Replace the ? with your favorite number.
+# Choose favorite number (Replace the question mark below)
 favNumber = ?
 
-# 
+# Ask for favorite college (Replace the question mark below with your function)
+favCollege = ?
+
+# Print message adding context to these variables (Replace the question mark below with your function)
+?
