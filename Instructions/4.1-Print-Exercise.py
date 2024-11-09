e7bf38e31d7395a02e9d1e993c713a8b10c81c18
Madison Thompson <53984561+madisonthompson27@users.noreply.github.com>
2024-11-09 15:15:15 -0500

    Create 4.1-Print-Exercise.py

---
+++ Instructions/4.1-Print-Exercise.py
@@ -0,0 +1,43 @@
+# Let's practice printing messages to your friends
+
+# example
+# a class example of printing in computer science is the "hello world"
+# message. Below, an example of hello world is written
+print("Hello world!")
+
+      
+# problem 1
+# in the space below, write a print statement that says Hello world!
+# tips: make sure you have two parenthesis, two quotes, and a message inside the quotes
+
+
+
+# example 2
+# sometimes, we want to print multiple things, or have a custom message
+# the code below shows you how you can print out a message to with a person's name
+name = "Madison" #P.S. what type is this variable? The line of code below will check
+
+print("The variable name has a type of : ", type(name))
+
+print("Hi, ", name)
+
+# problem 2
+# create a variable that is your name, or anything else that you want to print,
+# using the format shown above
+
+
+# example 3
+# you can print as many things as you want in one print statement-- like the example below
+school = "NC State"
+mascot = "Wolfpack"
+sport = "football"
+students = 40000
+
+print("The best school ever is the ", school, " ", mascot, "and they're really good at ", sport, 
+     "and they have ", students, " students")
+
+# in the example above, what happens if you remove the commas? the space between school and mascot? 
+# there's nothing wrong with causing errors in the code, it's how you learn!
+
+# problem 3
+# using the example above, build your own sentence with various types!
