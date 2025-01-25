52afdf748380636c4e6ba69c2cf5270f0d680aed
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-25 18:01:47 -0500

    Update Python.py

--- StarterCode/Python.py
+++ StarterCode/Python.py
@@ -1,7 +1,7 @@
 # Choose favorite number (Replace the question mark below)
 favNumber = ?
 
-# Ask for favorite college (Replace the question mark below with your function)
+# Ask the user for their favorite college (Replace the question mark below with your function)
 favCollege = ?
 
 # Print message adding context to these variables (Replace the question mark below with your function)
