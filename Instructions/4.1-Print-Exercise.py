51a10cc4ccfedce4cad519a5ffe7200a030ff840
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-25 17:19:40 -0500

    Update 4.1-Print-Exercise.py

--- Instructions/4.1-Print-Exercise.py
+++ Instructions/4.1-Print-Exercise.py
@@ -1,33 +1,33 @@
-# Let's practice printing messages to your friends
+# Let's practice printing messages to your friends!
 
-# example
-# a class example of printing in computer science is the "hello world"
-# message. Below, an example of hello world is written
+# Example
+# A class example of printing in computer science is the "hello world"
+# message. Below, an example of hello world is written.
 print("Hello world!")
 
       
-# problem 1
-# in the space below, write a print statement that says Hello world!
-# tips: make sure you have two parenthesis, two quotes, and a message inside the quotes
+# Problem 1
+# In the space below, write a print statement that says Hello world!
+# Tips: make sure you have two parenthesis, two quotes, and a message inside the quotes.
 
 
 
-# example 2
-# sometimes, we want to print multiple things, or have a custom message
-# the code below shows you how you can print out a message to with a person's name
-name = "Madison" #P.S. what type is this variable? The line of code below will check
+# Example 2
+# Sometimes, we want to print multiple things, or have a custom message.
+# The code below shows you how you can print out a message with a person's name.
+name = "Madison" #P.S. what type is this variable? The line of code below will check.
 
 print("The variable name has a type of : ", type(name))
 
 print("Hi, ", name)
 
-# problem 2
-# create a variable that is your name, or anything else that you want to print,
-# using the format shown above
+# Problem 2
+# Create a variable that is your name, or anything else that you want to print,
+# using the format shown above.
 
 
-# example 3
-# you can print as many things as you want in one print statement-- like the example below
+# Example 3
+# You can print as many things as you want in one print statement-- like the example below.
 school = "NC State"
 mascot = "Wolfpack"
 sport = "football"
@@ -36,8 +36,8 @@ students = 40000
 print("The best school ever is the ", school, " ", mascot, "and they're really good at ", sport, 
      "and they have ", students, " students")
 
-# in the example above, what happens if you remove the commas? the space between school and mascot? 
-# there's nothing wrong with causing errors in the code, it's how you learn!
+# In the example above, what happens if you remove the commas? the space between school and mascot? 
+# There's nothing wrong with causing errors in the code; it's how you learn!
 
-# problem 3
-# using the example above, build your own sentence with various types!
+# Problem 3
+# Using the example above, build your own sentence with various types!
