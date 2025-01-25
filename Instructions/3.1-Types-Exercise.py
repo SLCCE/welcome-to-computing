7597125d0d66e1a917805e431d63334db5472200
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-25 17:17:16 -0500

    Update 3.1-Types-Exercise.py

--- Instructions/3.1-Types-Exercise.py
+++ Instructions/3.1-Types-Exercise.py
@@ -1,27 +1,27 @@
-# for all of the problems below, predict what value will be printed out
+# For all of the problems below, predict what value will be printed out.
 
-# problem 1
+# Problem 1
 print("The type of \"SLCCE of awesome!\" is: ", type("SLCCE is awesome!"))
 
-# problem 2
+# Problem 2
 print("The type of \"name\" is: ", type("name"))
 
-# problem 3
+# Problem 3
 print("The type of 97 is: ", type(97))
 
-# problem 4
+# Problem 4
 print("The type of 9.7 is: ", type(9.7))
 
-# problem 5
+# Problem 5
 print("The type of 97.0 is: ", type(97.0))
 
-# problem 6
+# Problem 6
 print("The type of \"97.0\" is: ", type("97.0"))
 
-# problem 7
+# Problem 7
 print("The type of -1 is: ", type(-1))
 
-# problem 8
+# Problem 8
 print("The type of -1.0 is: ", type(-1.0))
 
-# can you write your own problems to test yourself, friends, family, or group members?
+# Can you write your own problems to test yourself, friends, family, or group members?
