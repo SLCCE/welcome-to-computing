8e275fbe834c2a29b551915ffa845cbf8536f688
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-25 18:20:58 -0500

    Create Java.java

---
+++ StarterCode/Java.java
@@ -0,0 +1,17 @@
+public class Java {
+  public static void main(String[] args) {
+    // Create Scanner for user input (Replace the question mark below)
+    ? in = new Scanner(System.in);
+
+    // Ask for favorite animal (Replace the question marks below)
+    System.out.print(?);
+    ? favAnimal = ?;
+
+    // Ask for favorite number (Replace the question marks below)
+    System.out.print(?);
+    ? favNumber = ?;
+
+    // Print message adding context to these variables (Replace the question mark below)
+    ?
+  }
+}
