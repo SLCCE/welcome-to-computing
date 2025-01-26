5a171ea1487ce52d51de5fe91823985d85de81bf
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-26 18:43:23 -0500

    Create 7.1-Java-Syntax-Exercise.java

---
+++ Instructions/7.1-Java-Syntax-Exercise.java
@@ -0,0 +1,24 @@
+public class JavaSyntaxExercise {
+  public static void main(String[] args) {
+    // Problem 1
+    // In the space below, include a single-line comment with the message "Hello, it's me!"
+
+    
+    // Problem 2
+    // In the space below, include a multi-line comment stating what you are most excited to learn about coding.
+
+
+
+
+    
+    // Problem 3
+    // In the space below, include a comment of your choice stating what must be at the end of every statementin Java.
+
+    
+
+    
+
+    // When running this code, the below line should print if there are no errors.
+    System.out.println("Finished!");
+  }
+}
