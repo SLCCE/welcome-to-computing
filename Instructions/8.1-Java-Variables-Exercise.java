f85740a796628ce99fe106f764b72fb1398c7359
jmlevy23 <72579891+jmlevy23@users.noreply.github.com>
2025-01-26 19:30:57 -0500

    Create 8.1-Java-Variables-Exercise.java

---
+++ Instructions/8.1-Java-Variables-Exercise.java
@@ -0,0 +1,27 @@
+public class JavaVariablesExercise {
+  public static void main(String[] args) {
+    // Problems 1-5
+    // You'll declare several variables in the space below.
+
+    // Define a character named c that stores the letter 'f'.
+
+
+    // Define an integer named i that stores the number 15.
+
+
+    // Define a double named pi that stores the number 3.14.
+
+
+    // Define a String named emotion that stores the text "Happy".
+
+
+
+    // Problems 6-7
+    // Write a print statement below that prints the message "Wow!" without including a new line.
+
+
+    // Now, write a print statement below that prints the message "Ooh!" including a new line.
+
+    
+  }
+}
