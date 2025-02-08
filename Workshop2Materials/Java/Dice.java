e256172b9fb29ecb388c10a2ee02761ea13cc4fd
Jason Yin <jyin23@ncsu.edu>
2025-02-07 23:19:03 -0500

    init WS2 file structure, java toString demo (exercise later?)

---
+++ Workshop2Materials/Java/Dice.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Dice {
+    private int sides;
+    Random rand;
+    
+    Dice(int sides) {
+        this.sides = sides;
+        this.rand = new Random();
+    }
+
+    private int roll() {
+        int val = this.rand.nextInt(sides) + 1;
+        return val;
+    }
+
+    // @Override
+    // public String toString() {
+    //     return "Sides: " + sides;
+    // }
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        
+        // roll the dice
+        Dice d6 = new Dice(6);
+        int rollValue = d6.roll();
+        System.out.println(rollValue);
+
+        // try to print out the dice object
+        System.out.println(d6);
+    }
+}
+
+
