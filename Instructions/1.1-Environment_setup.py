87e079f74bbade6d7811db89311729f3618a63bc
erichbopp <118788041+erichbopp@users.noreply.github.com>
2025-02-14 16:29:50 -0500

    Create 1.1-Environment_setup.py

---
+++ Instructions/1.1-Environment_setup.py
@@ -0,0 +1,18 @@
+'''
+Github: Web-based version control and collaboration platform for software development.
+'''
+
+'''
+Integrated Development Environment (IDE): An IDE is a software that combines commonly 
+used developers tools into a compact User Interface application to help programmers 
+develop code efficiently
+'''
+
+'''
+Terminal: A computer terminal is a device that allows users to interact with a computer
+using a keyboard and display.
+'''
+
+'''
+Intellisense: 
+'''
