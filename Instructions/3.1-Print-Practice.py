231b1d2342e4332ad40287c9466d948adeb653ce
Madison Thompson <53984561+madisonthompson27@users.noreply.github.com>
2025-02-14 15:56:36 -0500

    Create 3.1-Print-Practice.py

---
+++ Instructions/3.1-Print-Practice.py
@@ -0,0 +1,25 @@
+'''
+Use this exercise file to practice printing to the console in Python!
+'''
+
+# Example 1: The following code will print out the school that SLCCE was founded at!
+print( "North Carolina State University" )
+
+
+# Problem 1: Write some code to print out your name
+
+
+# Problem 2: Write some code to print out your favorite color
+
+
+# Problem 3: Use two print statements to print your name and someone else's
+
+
+
+'''
+Question: How could we print multiple phrases with the same print statements?
+
+Your Thoughts:
+
+
+'''
