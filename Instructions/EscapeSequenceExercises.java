d0557b8c98a7c29330d26d23c8dbdfbd023526df
Eshan Srivastav <eshansrivastav05@gmail.com>
2025-04-13 19:27:14 -0400

    Added 7.2 & 7.3 - Escape Characters in Java

---
+++ Instructions/EscapeSequenceExercises.java
@@ -0,0 +1,35 @@
+public class EscapeSequenceExercises {
+
+    public static void main(String[] args) {
+        // Escape Sequence Exercises
+        // Try to guess the output of each function before running it!//
+
+        // Exercise 1: Newline
+        System.out.println("Roses are red,\nViolets are blue,\nCoding is fun,\nAnd so are you!\n");
+
+        // Exercise 2: Tabs
+        System.out.println("Name\tGrade\tAge");
+        System.out.println("John\tA\t16");
+        System.out.println("Sara\tB\t17\n");
+        
+        // Exercise 3: Quotes inside strings
+        System.out.println("She said, \"Java is cool!\"");
+        System.out.println("He replied, \"Absolutely.\"\n");
+        
+        // Exercise 4: Backslashes
+        System.out.println("File path: C:\\Program Files\\Java\\jdk-17");
+        System.out.println("Escape the \\ like this: \\\\\n");
+
+        // Exercise 5: Single Quotes and Unicode
+        System.out.println("It\'s a great day to code!");
+        System.out.println("Here\'s a heart symbol using Unicode: \u2764\n");
+
+        // Exercise 6: Carriage Return
+        System.out.print("12345\rABCDE");
+        System.out.println("\n");
+
+        // Exercise 7: Backspace
+        System.out.println("Oops\b!");
+        System.out.println("Tricky\b\b\b\bRight?\n");
+    }
+}
