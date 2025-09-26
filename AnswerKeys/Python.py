8e791c8b40a4841c3f693c1ea20db8ff9f241fe3
Jason Yin <jyin23@ncsu.edu>
2025-09-26 16:33:40 -0400

    rm Java for consistency

--- AnswerKeys/Python.py
+++
@@ -1,8 +0,0 @@
-# Choose favorite number
-favNumber = 30
-
-# Ask for favorite college
-favCollege = input("What is your favorite college or university? ")
-
-# Print message adding context to these variables
-print("My favorite college is", favCollege, "and my favorite number is," favoriteNumber, ".")
--- StarterCode/Python.py
+++
@@ -1,8 +0,0 @@
-# Choose favorite number (Replace the question mark below)
-favNumber = ?
-
-# Ask the user for their favorite college (Replace the question mark below with your function)
-favCollege = ?
-
-# Print message adding context to these variables (Replace the question mark below with your function)
-?
